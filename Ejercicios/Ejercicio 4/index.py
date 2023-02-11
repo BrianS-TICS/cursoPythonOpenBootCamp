e1476ca9b4d826f3bee441b24fd2a79fd5fb8e96
Brian Sanchez Aguilar <briansanchezaguilar@gmail.com>
2023-02-10 23:02:19 -0700

    add Estructuras de control y ejercicio 4

---
+++ Ejercicios/Ejercicio 4/index.py
@@ -0,0 +1,5 @@
+# Escribe un programa que sea capaz de mostrar los números del 1 al 100 en orden inverso.
+i = 1
+while i != 101 : 
+    print(i)
+    i += 1
---
+++ Estructuras de control/index.py
@@ -0,0 +1,19 @@
+# Estructuras de control
+a = 5
+b = 6
+c = 10
+
+# Operaciones condicionales
+# > Moyor
+# >= Moyor o igual
+# == Exactamente igual
+# <= Menor o igual
+# < Menor
+
+resultado = (a == b)
+
+print(resultado)
+
+# Palabras reservadas
+resultadoWithAnd = (a < 2 and a > c)
+print(resultadoWithAnd)
