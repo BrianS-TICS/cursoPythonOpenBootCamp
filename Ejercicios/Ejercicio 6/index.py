15bbb8ed5d7692424b7b7d79ade5373ffc848797
BrianS-TICS <briansanchezaguilar@gmail.com>
2023-02-16 22:48:18 -0700

    Se actualiza solucion a ejercicio 6

--- Ejercicios/Ejercicio 6/index.py
+++ Ejercicios/Ejercicio 6/index.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 class Vehiculo:
     color = ""
     ruedas = 0
@@ -8,10 +10,6 @@ class Vehiculo:
         self.ruedas = ruedas
         self.puertas = puertas
 
-    def __str__(self):
-        return ("color: " + self.color, " ruedas: " + self.ruedas + " puertas: " + self.puertas)
-
-
 class Coche(Vehiculo):
     velocidad = 0
     cilindrada = 0
@@ -21,8 +19,6 @@ class Coche(Vehiculo):
         self.velocidad = velocidad
         self.cilindrada = cilindrada
 
-    def __str__(self) :
-        return super(Coche, self).__str__() + " velocidad : " + f'{self.velocidad}' + "  cilindrada : " + f'{self.cilindrada}'
-
 nissan=Coche('Negro', 4, 4, 255, 8)
-print(nissan.__str__())
+
+pprint(vars(nissan))
