6beb4037d56cf12786462607b461f03253940e62
BrianS-TICS <briansanchezaguilar@gmail.com>
2023-02-16 23:46:29 -0700

    Se agrega inicio de modulos

---
+++ Ejecucion de modulos/demo-module/main.py
@@ -0,0 +1,13 @@
+import operaciones as op
+import welcome
+
+
+def main():
+    welcome
+    op.suma(2, 2)
+    op.showName()
+    print(op.PI)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Ejecucion de modulos/demo-module/operaciones.py
@@ -0,0 +1,7 @@
+PI = 3.1416
+
+def suma(x, y) :
+    print(x + y)
+    
+def showName() :
+    print(__name__)
